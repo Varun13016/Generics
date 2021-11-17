5cde8a12af92e59bec809ec3cee171c9c9f74108
“Varun13016” <dmevarun@gmail.com>
2021-11-17 18:43:22 +0530

    MaximumProblem-uc3

---
+++ MaximumProblem-uc3/MaxString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumProblem_uc3
+{
+    class MaxString
+    {
+        static String Max(String FirstString, String SecondString, String ThirdString)
+        {
+            if (FirstString.CompareTo(SecondString) > 0 && FirstString.CompareTo(ThirdString) > 0 ||
+                FirstString.CompareTo(SecondString) >= 0 && FirstString.CompareTo(ThirdString) > 0 ||
+                FirstString.CompareTo(SecondString) > 0 && FirstString.CompareTo(ThirdString) >= 0)
+            {
+                return FirstString;
+            }
+
+            if (SecondString.CompareTo(FirstString) > 0 && SecondString.CompareTo(ThirdString) > 0 ||
+                SecondString.CompareTo(FirstString) >= 0 && SecondString.CompareTo(ThirdString) > 0 ||
+                SecondString.CompareTo(FirstString) > 0 && SecondString.CompareTo(ThirdString) >= 0)
+            {
+                return SecondString;
+            }
+
+            if (ThirdString.CompareTo(FirstString) > 0 && ThirdString.CompareTo(SecondString) > 0 ||
+                ThirdString.CompareTo(FirstString) >= 0 && ThirdString.CompareTo(SecondString) > 0 ||
+                ThirdString.CompareTo(FirstString) > 0 && ThirdString.CompareTo(SecondString) >= 0)
+            {
+                return ThirdString;
+            }
+            return FirstString;
+        }
+    }
+}
---
+++ MaximumProblem-uc3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaximumProblem_uc3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
