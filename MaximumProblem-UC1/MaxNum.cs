0c0cbd61cfda8af34eb132fad122ad3c6cb25ca9
“Varun13016” <dmevarun@gmail.com>
2021-11-17 18:36:22 +0530

    max-uc1

---
+++ MaximumProblem-UC1/MaxNum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumProblem_UC1
+{
+    class MaxNum
+    {
+        static int MaxIntNum(int a, int b, int c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 ||
+                c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 ||
+                c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+            }
+            return a;
+        }
+    }
+}
---
+++ MaximumProblem-UC1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumProblem_UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
