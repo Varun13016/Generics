31a6ffcb87ed2ec9677c60bfd2a8216c452a477f
“Varun13016” <dmevarun@gmail.com>
2021-11-17 18:39:48 +0530

    Max-uc2

---
+++ MaximumProblem-uc2/MaxFloat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumProblem_uc2
+{
+    class MaxFloat
+    {
+        static double Max(double FirstNum, double SecondNum, double ThirdNum)
+        {
+            if (FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) > 0 ||
+                FirstNum.CompareTo(SecondNum) >= 0 && FirstNum.CompareTo(ThirdNum) > 0 ||
+                FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) >= 0)
+            {
+                return FirstNum;
+            }
+
+            if (SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0 ||
+                SecondNum.CompareTo(FirstNum) >= 0 && SecondNum.CompareTo(ThirdNum) > 0 ||
+                SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) >= 0)
+            {
+                return SecondNum;
+            }
+            if (ThirdNum.CompareTo(FirstNum) > 0 && ThirdNum.CompareTo(SecondNum) > 0 ||
+                ThirdNum.CompareTo(FirstNum) >= 0 && ThirdNum.CompareTo(SecondNum) > 0 ||
+                ThirdNum.CompareTo(FirstNum) > 0 && ThirdNum.CompareTo(SecondNum) >= 0)
+            {
+                return ThirdNum;
+            }
+            return FirstNum;
+        }
+    }
+}
